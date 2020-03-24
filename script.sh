5ecb00b9d014d48ddb1316c98d9dfdf066d19407
kadmiv <kachulyak.ivan@gmail.com>
2020-03-24 08:32:45 +0200

    add script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Service Solver"
+export JAVA_OPTS="-Xmx10G"
+#java -jar ./build/libs/solver_nn-1.0.1.jar -Xmx10G --server.port=5550
+./gradlew bootRun --args='--server.port=5550 --spring.profiles.active=prod'
+exit 0
