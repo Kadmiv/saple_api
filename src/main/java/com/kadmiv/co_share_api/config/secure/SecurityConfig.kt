ed3c60e365a20a922eb391b3da5b9060055b4b9e
kadmiv <kachulyak.ivan@gmail.com>
2020-03-25 22:02:36 +0200

    add auth request

--- src/main/java/com/kadmiv/co_share_api/config/secure/SecurityConfig.kt
+++ src/main/java/com/kadmiv/co_share_api/config/secure/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package com.kadmiv.co_share_api.config.secure
 
 import com.kadmiv.co_share_api.controllers.AUTH_PATH
+import com.kadmiv.co_share_api.controllers.REGISTRATION_PATH
 import com.kadmiv.co_share_api.repo.user.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
@@ -29,7 +30,10 @@ open class SecurityConfig : WebSecurityConfigurerAdapter() {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/$AUTH_PATH").permitAll()
+                .antMatchers(
+//                        "/$AUTH_PATH",
+                        "/$AUTH_PATH/$REGISTRATION_PATH"
+                ).permitAll()
                 .antMatchers("/test").permitAll()
                 .anyRequest().authenticated()
                 .and()
